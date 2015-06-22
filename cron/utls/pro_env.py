6f8efe82bcac16b7b4d91e0ce48d732ebfb02169
tinylcy <tinylcy@yeah.net>
2015-06-22 14:23:52 +0800

    环境变量

---
+++ cron/utls/pro_env.py
@@ -0,0 +1,30 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/python
+# FileName:pro_env.py
+#***************************************************
+# 项目的路径
+PROJECT_DIR = "/usr/local/EclipseProjects/MyBI"
+# 项目配置文件的路径
+PROJECT_CONF_DIR = PROJECT_DIR + "/conf/"
+# 项目第三方库的路径
+PROJECT_LIB_DIR = PROJECT_DIR + "/lib"
+# 项目临时文件的路径
+PROJECT_TMP_DIR = PROJECT_DIR + "/temp"
+#***************************************************
+
+# Hadoop的安装路径
+HADOOP_HOME = "/usr/local/hadoop/"
+# Hadoop的命令路径
+HADOOP_PATH = HADOOP_HOME + "bin/"
+# HIVE的安装路径
+HIVE_HOME = "/opt/hive-0.9.0/"
+# HIVE的命令路径
+HIVE_PATH = HIVE_HOME + "bin/"
+# Sqoop的安装路径
+SQOOP_HOME = "/opt/Sqoop/"
+# Sqoop的命令路径
+SQOOP_PATH = SQOOP_HOME + "bin/"
+#***************************************************
+
+# Java的安装路径
+Java_HOME = "/usr/lib/jvm/jdk1.7.0_75"
